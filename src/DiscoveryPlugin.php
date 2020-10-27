6b8501274519e4a1da42c9fdc85ab06969bf6da2
recca0120 <recca0120@gmail.com>
2020-10-27 17:50:20 +0800

    composer 2

--- src/DiscoveryPlugin.php
+++ src/DiscoveryPlugin.php
@@ -94,4 +94,14 @@ class DiscoveryPlugin implements PluginInterface, EventSubscriberInterface, Capa
             CommandProvider::class => DiscoveryCommandProvider::class,
         ];
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement deactivate() method.
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement uninstall() method.
+    }
 }
